3e325333bc8d80897d8d87c18fb10b605fefa2ce
Fr0ZZZT <newkosss@gmail.com>
2020-06-17 15:06:03 +0300

    unittest style registration1 code

--- lesson3_step2_13.py
+++ lesson3_step2_13.py
@@ -1,34 +1,27 @@
 import unittest
 from selenium import webdriver
 
-link = "http://suninjuly.github.io/registration2.html"
 browser = webdriver.Chrome(executable_path="C:\chromedriver\chromedriver.exe")
-browser.get(link)
 
 class TestRegistration1(unittest.TestCase):
-    def test_input_field1(self):
-        self.assertEqual(browser.find_element_by_css_selector('.first_block .first').send_keys("Constantine"),
-        f"Failed to input First name")
-
-    def test_input_field2(self):
-        self.assertEqual(browser.find_element_by_css_selector('.first_block .second').send_keys("Fr0ZT"),
-        f"Failed to input Last name")
-
-    def test_input_field3(self):
-        self.assertEqual(browser.find_element_by_css_selector('.first_block .third').send_keys("newkosss@gmail.com"),
-        f"Failed to input First Email")
-
-    def test_input_field4(self):
-        self.assertEqual(browser.find_element_by_css_selector('.second_block .first').send_keys("102"),
-        f"Failed to input Phone")
-
-    def test_input_field5(self):
-        self.assertEqual(browser.find_element_by_css_selector('.second_block .second').send_keys("Odessa"),
-        f"Failed to input Address")
-
-    def test_input_button1(self):
-        self.assertEqual(browser.find_element_by_css_selector("button.btn").click(),
-        f"Failed to click on button")
+    def test_input1(self):
+        link = "http://suninjuly.github.io/registration1.html"
+        browser.get(link)
+        input1 = browser.find_element_by_css_selector('.first_block .first')
+        input1.send_keys("Constantine")
+        input2 = browser.find_element_by_css_selector('.first_block .second')
+        input2.send_keys("Fr0ZT")
+        input3 = browser.find_element_by_css_selector('.first_block .third')
+        input3.send_keys("newkosss@gmail.com")
+        input4 = browser.find_element_by_css_selector('.second_block .first')
+        input4.send_keys("102")
+        input4 = browser.find_element_by_css_selector('.second_block .second')
+        input4.send_keys("Odessa")
+        button = browser.find_element_by_css_selector("button.btn")
+        button.click()
+        welcome_text_elt = browser.find_element_by_tag_name("h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, welcome_text_elt.text, "Congrats text should be equal")
 
 
 if __name__ == "__main__":
